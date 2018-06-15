9e629ba4eeba79025cf757884ad358957453abab
anlob <andreas.lobbes@gmail.com>
2018-06-15 22:32:28 +0200

    gcc RTL exception granted to copyright holder

--- src/libtrapi.cpp
+++ src/libtrapi.cpp
@@ -2,7 +2,8 @@
  *
  * \author Andreas Lobbes, andreas.lobbes@gmail.com
  * \date 2018
- * \copyright GPLV3 and above
+ * \copyright GPLV3 and above + GCC RTL Exception 3.1
+ * \copyright Exception applies to any former and current copyright holder
  * \see http://www.gnu.org/licenses
  */
 #include "libtrapi.h"
