fe0f0dcd39024128d887b912ac48a8ca4a7704ec
anlob <andreas.lobbes@gmail.com>
2018-06-16 12:43:34 +0200

    copyright info in doxygen hdr reworked

--- src/common.h
+++ src/common.h
@@ -3,8 +3,9 @@
  *
  * \author Andreas Lobbes, andreas.lobbes@gmail.com
  * \date 2018
- * \copyright GPLV3 and above + GCC RTL Exception 3.1
- * \copyright Exception applies to any former and current copyright holder
+ * \copyright GPLV3 and above
+ * \copyright GCC RTL Exception 3.1 applies to any former and current copyright holder
+ * \see LICENSES at top level directory
  * \see http://www.gnu.org/licenses
  */
 #ifndef COMMON_H_INCLUDED
